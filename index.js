9cd43b23cd9a1a40e805fa9178bc9867ed317432
PankakeCoder <pankakeagain@gmail.com>
2022-02-21 13:12:32 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $ = (queryString) => document.querySelector(queryString);
 
-$('a-cyllinder').setAttribute('color','#FF0000');
+$('a-cylinder').setAttribute('color','#FF0000');
 $('a-box').setAttribute('color','#00FF00');
 $('a-plane').setAttribute('color','#808080');
