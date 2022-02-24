d886410ae50cb6a91c710f74b1471c7230698a29
PankakeCoder <pankakeagain@gmail.com>
2022-02-24 18:25:04 -0500

    Initial commit

--- Index.js
+++ Index.js
@@ -1,5 +1,7 @@
+var anyNumber = [...Array(10001).keys()]
+
 $ = (queryString) => document.querySelector(queryString);
 
-if($('#1').getAttribute('position') == '0 1 -1'){
+if($('#1').getAttribute('position') == 'anyNumber 1 anyNumber'){
     var win = new Audio("win.mp3");
 }
